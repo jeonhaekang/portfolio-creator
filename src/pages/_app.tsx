29890c9f5e27ca911261e5a521e85a35ce63a979
jeonhaekang <1003gorkd@naver.com>
2023-05-16 11:45:58 +0900

    initialize

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { GlobalCSS } from "@sun-river/components";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalCSS />
+      <Component {...pageProps} />
+    </>
+  );
 }
