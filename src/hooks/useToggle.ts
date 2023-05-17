01b0111d3624f3801f5760632fd9cd01e879f8b0
jeonhaekang <1003gorkd@naver.com>
2023-05-17 11:44:45 +0900

    fix: typescript 대응

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,9 +1,9 @@
 import { useCallback, useState } from "react";
 
 export const useToggle = (initState?: boolean) => {
-  const [isOpen, setIsOpen] = useState(initState);
+  const [isOpen, setIsOpen] = useState(initState || false);
 
   const onToggle = useCallback(() => setIsOpen(prev => !prev), []);
 
-  return [isOpen, onToggle];
+  return [isOpen, onToggle] as const;
 };
